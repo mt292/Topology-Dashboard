3bf44b25c583b5f9d2f05a624581137c4ac56f73
mt292 <mtopp887@gmail.com>
2025-04-27 21:10:13 -0400

    Imports in src/index.js
    import 'reactflow/dist/style.css';
    import 'reactflow/dist/theme-default.css';

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'react-flow-renderer/dist/style.css';
+import 'reactflow/dist/style.css';
+import 'reactflow/dist/theme-default.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
